c29fb1ceeeda5f92bb025d8356cac1e10e19d9b3
SriGaurangaDas <pranabbabu.nandi@gmail.com>
2025-09-24 12:55:02 +0530

    feat: Implement landlord registration endpoint with BDD/TDD

---
+++ src/test/java/com/rmage/rmage_backend/CucumberIntegrationTest.java
@@ -0,0 +1,15 @@
+package com.rmage.rmage_backend;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.rmage.rmage_backend.steps")
+public class CucumberIntegrationTest {
+}
